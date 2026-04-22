9e25db14a54fd71dd0f0286a097e3112001b7d88
Ishan <ishansuwal7@gmail.com>
2026-04-22 17:34:14 +0545

    Added get blog feature

--- app.js
+++ app.js
@@ -28,12 +28,21 @@ app.post("/blog", upload.single('image'), async(req, res)=>{
         title: title,
         subtitle: subtitle,
         description: description,
-        image: image.path
+        image: image.filename
     })
         res.status(200).json({
         message:"Blog api hit successfully "
     })
 })
+
+app.get("/blog", async(req,res)=>{
+   const blogs=await Blog.find() //returns array
+   res.status(200).json({
+    message:"Blogs fetched successfully",
+    data:blogs
+   })
+})
+app.use(express.static('./storage'))
 
 app.listen(process.env.PORT, ()=>{
     console.log("NodeJs has started")
